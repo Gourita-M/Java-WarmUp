b17e9cf2d5a2101541403950bbda05654ea7ff53
Gourita-M <mouad.gourita@gmail.com>
2026-09-01 15:17:32 +0100

    Project Folders

---
+++ src/main/java/org/example/warmup/WarmUpApplication.java
@@ -0,0 +1,13 @@
+package org.example.warmup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarmUpApplication {
+
+    public static void main(String[] args)
+    {
+        System.out.println("testing");
+    }
+}
