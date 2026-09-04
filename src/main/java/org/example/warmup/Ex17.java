325568ce7f9e4f202caad5f8cf07b8649f4b785f
Gourita-M <mouad.gourita@gmail.com>
2026-09-04 09:55:31 +0100

    Exercise 17

---
+++ src/main/java/org/example/warmup/Ex17.java
@@ -0,0 +1,21 @@
+package org.example.warmup;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Ex17 {
+
+    public void displayArray()
+    {
+    ArrayList<String> languages = new ArrayList<>();
+
+    languages.add("PHP");
+    languages.add("Java");
+    languages.add("C++");
+    languages.add("Python");
+
+    System.out.println(languages);
+    
+    }
+
+}
